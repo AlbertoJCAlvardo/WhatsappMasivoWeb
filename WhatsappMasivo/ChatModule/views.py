b4b2426975e96e4e0b862c96a6953e5600a7708e
AlbertoJCAlvardo <alberto.alvarado.sandoval@gmail.com>
2024-07-16 17:42:44 -0600

    Wh

--- WhatsappMasivo/ChatModule/views.py
+++ WhatsappMasivo/ChatModule/views.py
@@ -89,7 +89,8 @@ def whatsapp_webhook(request):
                             wamid = value['messages'][0]['id']
                             timestamp = value['messages'][0]['timestamp']
                             datetimes = datetime.fromtimestamp(int(timestamp))
-                            if datetime.today().hour != datetimes.hour:
+                            if datetime.today().hour != datetimes.hour or (datetime.today().minute != datetimes.minute >= 5):
+                                
                                 datetimes = datetime.fromtimestamp(int(timestamp)) - timedelta(hours=6)
                             destiny = value['metadata']['display_phone_number']
                             message_type = value['messages'][0]['type']
